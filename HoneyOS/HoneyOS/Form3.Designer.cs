deb831d555267b75460b94cdb6f2528cb2de654c
Zayne Peladas <zapeladas@up.edu.ph>
2024-04-13 17:22:14 +0800

    commit remaining changes for file manager

--- HoneyOS/HoneyOS/Form3.Designer.cs
+++ HoneyOS/HoneyOS/Form3.Designer.cs
@@ -165,7 +165,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(77, 77);
             this.button2.TabIndex = 3;
-            this.button2.Text = "File Manager";
+            this.button2.Text = "File Manager ";
             this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
