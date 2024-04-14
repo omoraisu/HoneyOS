809551faab3a0d710fd1ead1e20c81d825cb347c
Charl Rey Garcia <charlreyg@gmail.com>
2024-04-14 12:38:34 +0800

    speech recognition engine improved (?)

--- HoneyOS/HoneyOS/Form3.cs
+++ HoneyOS/HoneyOS/Form3.cs
@@ -10,21 +10,175 @@ namespace HoneyOS
     {
         List<string> phrases = new List<string>
         {
+            /* command initializer */
             "honey",
-            // full commands
-            "open notepad please",
-            "close notepad please",
-            "shut down please",
+            /* full commands */
+            "open notepad please",          // create instance of notepad window
+            "open file manager please",     // create instance of file manager window
+            "close notepad please",         // close all existing instance of notepad window
+            "close file manager please",    // close all existing instance of file manager window
+            "time to sleep",                // close application
         };
 
         bool isListeningForAction;
+        SpeechRecognitionEngine recognizer;
+
         List<Form7> notepads = new List<Form7>();
+        List<Form5> file_managers = new List<Form5>();
 
         public Desktop()
         {
             InitializeComponent();
             isListeningForAction = false;
+
+            this.GotFocus += Desktop_GotFocus;
+            this.LostFocus += Desktop_LostFocus;
         }
+        private void Desktop_Load(object sender, EventArgs e)
+        {
+            notepadToolStripMenuItem.Visible = false;
+            BatteryTimer.Start();
+            label1.Text = DateTime.Now.ToShortTimeString();
+            label2.Text = DateTime.Now.ToShortDateString();
+
+            SpeechRecognition_Load();
+        }
+
+        private void Desktop_GotFocus(object sender, EventArgs e)
+        {
+            // add stuff to do whenever the desktop is currently focused
+            Debug.WriteLine("desktop is currently focused");
+            if (recognizer == null)
+            {
+                SpeechRecognition_Load();
+            }
+
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            Debug.WriteLine("speech is continued");
+        }
+        private void Desktop_LostFocus(object sender, EventArgs e)
+        {
+            // add stuff to do whenever the desktop has lost focused ie another window is currently focused
+            Debug.WriteLine("desktop has lost focus");
+            if (recognizer != null)
+            {
+                recognizer.RecognizeAsyncStop();
+                Debug.WriteLine("speech is paused");
+            }
+        }
+
+        private void SpeechRecognition_Load()
+        {
+            //setup grammar
+            Choices choices = new Choices(phrases.ToArray());
+            GrammarBuilder builder = new GrammarBuilder(choices);
+            Grammar grammar = new Grammar(builder);
+
+            // initializing Speech Recognition
+            recognizer = new SpeechRecognitionEngine();
+            recognizer.SetInputToDefaultAudioDevice();
+            recognizer.LoadGrammar(grammar);
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+        }
+
+        /* Speech Commands Functions */
+        private void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Text.ToLower() == "honey" && !isListeningForAction)
+            {
+                //indicate to UI that Beebot is listening
+                Debug.WriteLine("i heard u honey, what can I do for you?");
+                isListeningForAction = true;
+            }
+            else if (isListeningForAction)
+            {
+                isListeningForAction = false;
+
+                switch (e.Result.Text.ToLower()) // for each case, create a corresponding function
+                {
+                    case "open notepad please":
+                        Debug.WriteLine("sure, i'll open it for u");
+                        OpenNotepadFunction();
+                        break;
+                    case "open file manager please":
+                        Debug.WriteLine("sure, i'll open it for u");
+                        OpenFileManagerFunction();
+                        break;
+                    case "close notepad please":
+                        Debug.WriteLine("sure, i'll close it for u");
+                        CloseNotepadFunction();
+                        break;
+                    case "close file manager please":
+                        Debug.WriteLine("sure, i'll close it for u");
+                        CloseFileManagerFunction();
+                        break;
+                    case "time to sleep":
+                        Debug.WriteLine("sure, sweet dreams honey");
+                        ShutdownFunction();
+                        break;
+                    default:
+                        //indicate to UI that the command taken was not recognized
+                        Debug.WriteLine("I'm sorry honey, I'm not sure I heard you clearly");
+                        isListeningForAction = true;
+                        break;
+                }
+            }
+
+        }
+        private void OpenNotepadFunction()
+        {
+            notepadToolStripMenuItem.Visible = true;
+            // Create an instance of Form7
+            Form7 form7 = new Form7(this);
+            notepads.Add(form7);
+            form7.Show(this);
+        }
+        private void OpenFileManagerFunction()
+        {
+            notepadToolStripMenuItem.Visible = true;
+            // Create an instance of Form5
+            Form5 form5 = new Form5(this);
+            file_managers.Add(form5);
+            form5.Show(this);
+        }
+        private void CloseNotepadFunction()
+        {
+            foreach(Form7 notepad in notepads)
+            {
+                if (notepad.Visible)
+                {
+                    notepad.Hide();
+                    notepad.Dispose();
+                }
+            }
+            notepads.Clear();
+        }
+        private void CloseFileManagerFunction()
+        {
+            foreach (Form5 fm in file_managers)
+            {
+                if (fm.Visible)
+                {
+                    fm.Hide();
+                    fm.Dispose();
+                }
+            }
+            file_managers.Clear();
+        }
+        private void ShutdownFunction()
+        {
+            recognizer.Dispose();
+            Application.Exit();
+        }
+
+
+
+        /* Click / MouseEnter / MouseLeave Functions */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenNotepadFunction();
+        }
+
         // Handle the MouseEnter event to change the ToolStripMenuItem's image when hovered over
         private void startToolStripMenuItem_MouseEnter(object sender, EventArgs e)
         {
@@ -44,33 +198,6 @@ namespace HoneyOS
         {
 
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            OpenNotepadFunction();
-        }
-
-        private void Desktop_Load(object sender, EventArgs e)
-        {
-            notepadToolStripMenuItem.Visible = false;
-            BatteryTimer.Start();
-            label1.Text = DateTime.Now.ToShortTimeString();
-            label2.Text = DateTime.Now.ToShortDateString();
-
-            //setup grammar
-            Choices choices = new Choices(phrases.ToArray());
-            GrammarBuilder builder = new GrammarBuilder(choices);
-            Grammar grammar = new Grammar(builder);
-
-
-            // initializing Speech Recognition
-            SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
-            recognizer.SetInputToDefaultAudioDevice();
-            recognizer.LoadGrammar(grammar);
-            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
-            recognizer.RecognizeAsync(RecognizeMode.Multiple);
-        }
-
         private void notepadToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             OpenNotepadFunction();
@@ -85,70 +212,11 @@ namespace HoneyOS
             notepadToolStripMenuItem.Visible = false;
         }
 
-
-        private void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
-        {
-            if (e.Result.Text.ToLower() == "honey")
-            {
-                //indicate to UI that Beebot is listening
-                Debug.WriteLine("i heard u honey, what can I do for you?");
-                isListeningForAction = true;
-            }
-            else if (isListeningForAction)
-            {
-                isListeningForAction = false;
-
-                switch (e.Result.Text.ToLower()) // for each case, create a corresponding function
-                {
-                    case "open notepad please":
-                        Debug.WriteLine("sure, i'll open it for u");
-                        OpenNotepadFunction();
-                        break;
-                    case "close notepad please":
-                        Debug.WriteLine("sure, i'll close it for u");
-                        CloseNotepadFunction();
-                        break;
-                    case "shut down please":
-                        Debug.WriteLine("sure, goodbye honey");
-                        ShutdownFunction();
-                        break;
-                    default:
-                        //indicate to UI that the command taken was not recognized
-                        Debug.WriteLine("I'm sorry honey, I'm not sure I heard you clearly");
-                        isListeningForAction = true;
-                        break;
-                }
-            }
-
-        }
-        private void OpenNotepadFunction()
-        {
-            notepadToolStripMenuItem.Visible = true;
-            // Create an instance of Form7
-            Form7 form7 = new Form7(this);
-            notepads.Add(form7);
-            form7.Show();
-        }
-
-        private void CloseNotepadFunction()
-        {
-            foreach(Form7 notepad in notepads)
-            {
-                notepad.Hide();
-                notepad.Dispose();
-            }
-        }
-
-        private void ShutdownFunction()
-        {
-            Application.Exit(); // Close the application
-        }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            Form5 form5 = new Form5(this);
-            form5.Show();
+            OpenFileManagerFunction();
         }
+
         PowerStatus ps = SystemInformation.PowerStatus;
 
         private void BatteryTimer_Tick(object sender, EventArgs e)
