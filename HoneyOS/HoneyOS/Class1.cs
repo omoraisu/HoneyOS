c414c94c7242bcd2ded08d4bb8c05ba4013de68d
omoraisu <dapeladas@up.edu.ph>
2024-04-29 10:14:35 +0800

    create pcb class

---
+++ HoneyOS/HoneyOS/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoneyOS
+{
+    public class ProcessControlBlock
+    {
+        private int pID;
+        private int burstTime;
+        private int arrivalTime;
+        private int priority;
+        private status state;
+
+        public ProcessControlBlock(int pID, int burstTime, int arrivalTime, int priority, int state)
+        {
+            this.pID = pID;
+            this.burstTime = burstTime; 
+            this.arrivalTime = arrivalTime;
+            this.priority = priority;
+            this.state = (status)state;
+        }
+    }
+
+    public enum status
+    {
+        READY,
+        WAITING,
+        RUNNING,
+        TERMINATED
+    }
+}
